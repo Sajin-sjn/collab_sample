54b856454d488bce1389c1b3ca216c1ee439a312
Sajin PJ <sajinsjn4433@gmail.com>
2025-02-02 22:57:37 +0530

    user home page created

--- collabApp/urls.py
+++ collabApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('login',views.public_login,name='public_login'),
     path('signup',views.signup,name='signup'),
+    path('user_home',views.user_home,name='user_home'),
 ]
